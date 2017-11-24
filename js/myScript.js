540a2f0a9f5f8e48d91d14412f5599e592481a27
Ricardo Petronilho <rpetronilho98@gmail.com>
2017-11-24 22:45:00 +0000

    a

--- js/myScript.js
+++ js/myScript.js
@@ -31,3 +31,19 @@ function escapeHtml(text) {
 
 	return text.replace(/[&<>"']/g, function(m) { return map[m]; });
 }
+
+function getParameterByName(name) {
+    
+    var url = window.location.href;
+    
+    name = name.replace(/[\[\]]/g, "\\$&");
+    
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    
+    if (!results) return null;
+    
+    if (!results[2]) return '';
+    
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
